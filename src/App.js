19f515d8143acaf9d11ce428e9d35b57e3bfb28c
Mythcoder1016 <1399236287@qq.com>
2022-03-16 21:37:25 +0800

    选定antdesignUI框架

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { Button } from 'antd';
 import './App.css';
 
-function App() {
-  return (
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+   return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <audio autoPlay src="http://dl.stream.qqmusic.qq.com/C400002JJeYa2ODDwz.m4a?guid=5413400350&vkey=E9E8B9B475E39A843592C85A997078D97F4A9792ACFBAEFA13956E2AAD42B7A0DEFFC1A3EE2399CD614EB4CD9590D1BC9D3CDCAD9CD717D0&uin=1399236287&fromtag=66"/>
+      <Button type="primary">Button</Button>
     </div>
-  );
+   ) 
+  }
 }
 
 export default App;
