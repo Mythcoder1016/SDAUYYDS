a0627da95aa8ec660b9befa8395e1444af74988f
Mythcoder1016 <1399236287@qq.com>
2022-03-23 19:42:10 +0800

    测试提交

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React,{Component} from 'react';
-import { Button } from 'antd';
 import './App.css';
 
 
 
 class App extends Component{
-  constructor(props){
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            clientHeight: document.documentElement.clientHeight, // 屏幕高度
+        };
+    }
+    componentDidMount() {
+        window.addEventListener("resize", this.resize); //增加
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize); //取消
+    }
 
-  render() {
-   return(
-    <div className="App">
-      <audio autoPlay src="http://dl.stream.qqmusic.qq.com/C400002JJeYa2ODDwz.m4a?guid=5413400350&vkey=E9E8B9B475E39A843592C85A997078D97F4A9792ACFBAEFA13956E2AAD42B7A0DEFFC1A3EE2399CD614EB4CD9590D1BC9D3CDCAD9CD717D0&uin=1399236287&fromtag=66"/>
-      <Button type="primary">Button</Button>
-    </div>
-   ) 
-  }
+    resize=()=>{
+        this.setState({ clientHeight: document.documentElement.clientHeight }); //监听
+    }
+
+    render() {
+        return (
+            <div className="container" style={{ height: this.state.clientHeight }}></div>
+        )
+    }
 }
 
 export default App;
