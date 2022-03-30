7ca55c5a1ed5096db83f7364dd6317b44ddcc2e0
Mythcoder1016 <1399236287@qq.com>
2022-03-30 23:24:46 +0800

    背景视频铺满屏幕修复

--- src/App.js
+++ src/App.js
@@ -10,23 +10,12 @@ class App extends Component{
             clientHeight: document.documentElement.clientHeight, // 屏幕高度
         };
     }
-    componentDidMount() {
-        window.addEventListener("resize", this.resize); //增加
-    }
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.resize); //取消
-    }
-
-    resize=()=>{
-        this.setState({ clientHeight: document.documentElement.clientHeight }); //监听
-    }
 
     render() {
         return (
-            <>
-                <video src={mp4} loop className='' autoPlay muted/>
-                <div className="container" style={{ height: this.state.clientHeight }}></div>
-            </>
+            <div className='containWrap'>
+                <video src={mp4} loop autoPlay muted/>
+            </div>
         )
     }
 }
