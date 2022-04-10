349a9bce5d89e66ae92d03c71ff2d54c576ded9f
Mythcoder1016 <1399236287@qq.com>
2022-04-11 00:31:21 +0800

    更换背景，添加导航条

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,35 +1,53 @@
 import React,{Component} from 'react';
 import axios from 'axios';
 import "./Home.css";
-import mp4 from "../../assets/whalefall.mp4"
+import mp4 from "../../assets/DogWithDog.mp4"
+import 'animate.css';
+import { Menu } from 'antd';
+import { createFromIconfontCN } from '@ant-design/icons';
 
+
+
+const { SubMenu } = Menu;
+const IconFont = createFromIconfontCN({
+    scriptUrl: '//at.alicdn.com/t/font_2643293_1iq1ob9o2dn.js',
+});
 
 class Home extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            clientHeight: document.documentElement.clientHeight, // 屏幕高度
+            current: 'mail',
         };
     }
 
 
     componentDidMount=()=>{
-        axios.get("http://127.0.0.1:9090",{
-            params:{
-                username:"zhangsan",
-                password:123
-            }
-        }).then(value=>{
-            console.log(value.data);
-        }).catch(err=>{
-            console.log(err);
-        })
     }
 
+    handleClick = e => {
+        console.log('click ', e);
+        this.setState({ current: e.key });
+      };
     
     render() {
+        const  {current} = this.state;
         return (
             <div className='containWrap'>
+                <h1 className="animate__animated animate__flash animate__delay-2s title">{"MythCoder's Blog"}</h1>
+                <Menu className='NavMenu' onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+                    <SubMenu key="UniversityTime" icon={<IconFont type='icon-taikonglvke_chaiquan1'/>} title="大学时光">
+                        <Menu.Item key="setting:1">大一</Menu.Item>
+                        <Menu.Item key="setting:2">大二</Menu.Item>
+                        <Menu.Item key="setting:3">大三</Menu.Item>
+                        <Menu.Item key="setting:4">大四</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="PhotoOfCampus" icon={<IconFont type='icon-keji-'/>} title="校园一角">
+                        <Menu.Item key="setting:1">美景传送处</Menu.Item>
+                        <Menu.Item key="setting:2">开发中</Menu.Item>
+                    </SubMenu>
+                </Menu>
+                <div class="clear"></div>
                 <video src={mp4} loop autoPlay muted/>
             </div>
         )
@@ -37,3 +55,4 @@ class Home extends Component{
 }
 
 export default Home;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,20 @@ import "../index.css"
 const Home = lazy(()=>{return import("../pages/Home/Home")})
 
 export class MyRouter extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            clientHeight: document.documentElement.clientHeight, // 屏幕高度
+            clientWidth: document.documentElement.clientWidth, // 屏幕宽度
+        }
+    }
+
     render(){
+        let width = this.state.clientWidth/2;
+        let height = this.state.clientHeight/2;
         return(        
             <BrowserRouter>
-                <Suspense fallback={<Spin size="large" tip={"Loading..."} className="TipSpan"/>}>
+                <Suspense fallback={<Spin size="large" tip={"Loading..."} style={{position:"absolute",top:`${height-30}px`,left:`${width-30}px`}} />}>
                     <Routes>
                         <Route path="/" element={<Home/>}/>
                     </Routes>   
